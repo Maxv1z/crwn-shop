810ce601a6a1ef2c2d77613bd90f97ce441fe76a
Maxv1z <meitstudying@gmail.com>
2023-06-20 10:17:43 +0300

    created sign up component, the form input component, button component and styles to all of that

--- src/routes/sing-in/sign-in.component.jsx
+++ src/routes/sing-in/sign-in.component.jsx
@@ -1,19 +1,28 @@
-import { signInWithGooglePopup } from "../../utils/firebase/firebase.utils"
-import { createUserDocumentFromAuth } from "../../utils/firebase/firebase.utils";
+import { useEffect } from "react";
+import { getRedirectResult } from "firebase/auth";
+
+import {
+  auth,
+  signInWithGooglePopup,
+  createUserDocumentFromAuth
+} from "../../utils/firebase/firebase.utils";
+
+import SignUpForm from "../../components/sign-up-form/sign-up-form.component";
+
+const SignIn = () => {
+  const logGoogleUser = async () => {
+    const { user } = await signInWithGooglePopup();
+    const userDocRef = await createUserDocumentFromAuth(user);
+  };
 
 
-const SignIn =() => {
-    const logGoogleUser = async () => {
-        const {user} = await signInWithGooglePopup();
-        const userDocRef = await createUserDocumentFromAuth(user);
-    }
+  return (
+    <div>
+      <h1>Sign in Page</h1>
+      <button onClick={logGoogleUser}> Sign in with Google Popup</button>
+      <SignUpForm />
+    </div>
+  );
+};
 
-    return (
-        <div>
-            <h1>Sign in Page</h1>
-            <button onClick={ logGoogleUser }> Sign in with Google Popup</button>
-        </div>
-    )
-}
-
-export default SignIn
+export default SignIn;
